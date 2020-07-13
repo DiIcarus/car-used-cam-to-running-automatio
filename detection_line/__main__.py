9b8ad5f77b99fa779bc85bff8c636795f1ed457c
sonnlh.deephub <sonnlh@deephub.vn>
2020-07-14 00:16:37 +0700

    Preprocessing the frame
    - Get video by frame.
    - Preprocessing the frame and crop the need path.

--- detection_line/__main__.py
+++ detection_line/__main__.py
@@ -1,12 +1,17 @@
 import cv2
 from detection import preprocessImage
 import numpy as np
-cam_url = './demo/01 - Nen tham, duong thang.avi'
+
+cam_url = 'demo/04 - Nen go, duong cong, nhieu.avi'
 cap = cv2.VideoCapture(cam_url)
 while(True):
   ret, frame = cap.read()
   cv2.imshow("frame",frame)
-  preprocessImage(frame)
+  preim = preprocessImage(frame)
+  if preim is None:
+    print(preim)
+  else:
+    cv2.imshow("crop",preim)
   if cv2.waitKey(1) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
     break;
